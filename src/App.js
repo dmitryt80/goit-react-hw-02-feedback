723ac79c0edb0bcacf40c55adc8bf6cf992814cf
Dmitry Tartsaiev <dmitantar80@gmail.com>
2021-12-06 14:26:50 +0200

    add files and folders

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React, { Component } from 'react'
+import './App.css'
+import Container from './components/Container/Container'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Feedback from './components/Feedback/Feedback'
+import Statistics from './components/Statistics/Statistics'
+import Notification from './components/Notification/Notification'
+
+class App extends Component {
+  state = { good: 0, neutral: 0, bad: 0 }
+
+  onSetVoice = (type) => {
+    this.setState((prevState) => ({ [type]: prevState[type] + 1 }))
+  }
+
+  countTotalFeedback = () => {
+    const objKey = Object.keys(this.state)
+    return objKey.reduce((total, el) => (total += this.state[el]), 0)
+  }
+
+  countPositiveFeedbackPercentage = () => {
+    return Math.round((this.state.good / this.countTotalFeedback()) * 100)
+  }
+
+  render() {
+    const total = this.countTotalFeedback()
+    const positive = this.countPositiveFeedbackPercentage()
+    const state = this.state
+    const keys = Object.keys(this.state)
+    return (
+      <div className="App">
+        <Container title="Please leave feedback">
+          <Feedback options={keys} addVoice={this.onSetVoice} />
+        </Container>
+        <Container title="Statistics">
+          {total > 0 ? (
+            <Statistics
+              options={state}
+              total={total}
+              positivePercentage={positive}
+            />
+          ) : (
+            <Notification message="There is no feedback" />
+          )}
+        </Container>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
